67d9cf1718d56ebb93eaa4f84a2374817f8fe574
Dua555-maker <zxzxdua@gmail.com>
2026-01-12 09:59:17 +0700

    add edit_car template

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, flash, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = b'4564312348'
 
 cars = [
   {'id':1,'brand':'Toyota','model':'Yaris Ativ','year':2024,'price':695000},
@@ -29,5 +30,19 @@ def new_car():
     car = {'id':id,'brand':brand,'model':model,'year':year,'price':price}
 
     cars.append(car)
+    flash('Add new Car Successfully','success')
     return redirect(url_for('all_cars'))
   return render_template('cars/new_car.html',title='New Car Page')
+
+@app.route('/cars/<int:id>/delete')
+def delete_car(id):
+  for car in cars:
+    if id == car['id']:
+      cars.remove(car)
+      break
+  flash('Delete Car Successfully','success')
+  return redirect(url_for('all_cars'))
+
+@app.route('/cars/<int:id>/edit')
+def edit_car(id):
+  return render_template('cars/edit_car.html',title='Edit Car Page')
